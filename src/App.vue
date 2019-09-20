cb9a03a80d8913f5892d7c23f58a631135fa479d
Carlos Torras <carlos6torras@gmail.com>
2019-09-20 09:56:08 +0200

    copy of stress test with vue.cli

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vHeader v-model="search"></vHeader>
+    <BookLibrary :search="search" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import vHeader from "./components/Header";
+import BookLibrary from "./components/BookLibrary";
 export default {
-  name: 'app',
+  data() {
+    return {
+      search: ""
+    };
+  },
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    vHeader,
+    BookLibrary
+  },
+  methods: {}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
+  padding: 0px;
 }
 </style>
